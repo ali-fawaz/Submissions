651b48f389b7168d5533b92ce038a7e70333148e
Ali Fawaz <ali.fawzi.fawaz@gmail.com>
2019-12-17 09:49:14 +0200

    completed Step 5

---
+++ Exercises/Javascript-Basics-01/Step_5.js
@@ -0,0 +1,7 @@
+function multiplication() {
+  var first_number = document.getElementById("first_number").value;
+  var second_number = document.getElementById("second_number").value;
+  var mult = first_number * second_number;
+
+  alert("The product is :" + mult);
+}
