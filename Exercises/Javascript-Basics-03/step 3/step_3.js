e98483031b7a1a05637766edd1e8976fc2e967b2
Ali Fawaz <ali.fawzi.fawaz@gmail.com>
2019-12-18 16:21:49 +0200

    completed Step 3

---
+++ Exercises/Javascript-Basics-03/step 3/step_3.js
@@ -0,0 +1,11 @@
+function green() {
+  document.querySelector("#text").style.color = "green";
+}
+
+function red() {
+  text.style.color = "red";
+}
+
+function blue() {
+  text.style.color = "blue";
+}
