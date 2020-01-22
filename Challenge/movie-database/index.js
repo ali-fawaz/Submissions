814e3f59a3d3bd8b86b76305812e361bf387a049
Ali Fawaz <ali.fawzi.fawaz@gmail.com>
2020-01-22 12:38:08 +0200

    Step3

--- Challenge/movie-database/index.js
+++ Challenge/movie-database/index.js
@@ -3,5 +3,12 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => res.send("ok"));
+app.get("/test", (req, res) => res.send({ status: 200, message: "ok" }));
+app.get("/time", (req, res) => {
+  var d = new Date();
+  var t1 = d.getHours();
+  var t2 = d.getMinutes();
+  res.send({ status: 200, message: `${t1}:${t2}` });
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
